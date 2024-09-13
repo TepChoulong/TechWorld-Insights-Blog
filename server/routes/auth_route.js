2fe8f8da032dc6873b1034c56d203d1ba493c85c
TepChoulong <tepchoulongzin@gmail.com>
2024-09-13 12:50:43 +0700

    add verifying admin or moderator routes

--- server/routes/auth_route.js
+++ server/routes/auth_route.js
@@ -1,4 +1,6 @@
 import express from "express";
+import verifyAdminAccount from "../utils/verifyAdminAccount.js";
+import verifyModeratorAccount from "../utils/verifyModeratorAccount.js";
 
 // Controllers
 import {
@@ -9,9 +11,9 @@ import {
 
 const router = express.Router();
 
-router.get("/get-admin-account", getAdminAccount);
+router.get("/get-admin-account", verifyModeratorAccount, getAdminAccount);
 
 router.post("/login-for-admin", loginForAdmin);
-router.post("/add-admin-account", addAdminAccount);
+router.post("/add-admin-account", verifyAdminAccount, addAdminAccount);
 
 export default router;
